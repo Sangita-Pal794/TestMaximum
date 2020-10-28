05a9adabc0a20133d5d84300b8a78e4e6cd39f13
Sangita <pal07sangita@gmail.com>
2020-10-28 13:24:15 +0530

    Added: Method & Parameterized For The Max Value Among 3 Given Floats

--- src/test/java/TestMaximum/MaximumTest.java
+++ src/test/java/TestMaximum/MaximumTest.java
@@ -1,5 +1,20 @@
 package TestMaximum;
 
 public class MaximumTest {
-
+	
+	// Method to find the Max Integer
+	public Integer maxInteger(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+        if (num2.compareTo(num1) > 0) max = num2;
+        if (num3.compareTo(num2) > 0) max = num3;
+        return max;
+    }
+	
+	// Method to find the Max Float
+    public Float maxFloat(Float num1, Float num2, Float num3) {
+        Float max = num1;
+        if (num2.compareTo(num1) > 0) max = num2;
+        if (num3.compareTo(num2) > 0) max = num3;
+        return max;
+    }
 }
