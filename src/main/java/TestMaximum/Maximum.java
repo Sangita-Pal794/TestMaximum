970b36bc7ba82dba0c181f2e8b65941249c36dc0
Sangita <pal07sangita@gmail.com>
2020-10-28 21:46:43 +0530

    Added: Method & Parameterized For The Max Value Among 3 Given Floats

--- src/main/java/TestMaximum/Maximum.java
+++ src/main/java/TestMaximum/Maximum.java
@@ -1,61 +1,20 @@
 package TestMaximum;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.Collection;
-
-import static org.junit.Assert.assertEquals;
-
-@RunWith(Parameterized.class)
-
 public class Maximum {
 	
-	private Maximum testMax;
-    private Integer num1, num2, num3, output;
-
-    //public MaximumTest(Integer num1, Integer num2, Integer num3, Integer output) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
-        this.output = output;
+	// Method to find the Max Integer
+	public Integer maxInteger(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+        if (num2.compareTo(num1) > 0) max = num2;
+        if (num3.compareTo(num2) > 0) max = num3;
+        return max;
     }
-
-    @Before
-    public void setUp() {
-        testMax = new Maximum();
+	
+	// Method to find the Max Float
+    public Float maxFloat(Float num1, Float num2, Float num3) {
+        Float max = num1;
+        if (num2.compareTo(num1) > 0) max = num2;
+        if (num3.compareTo(num2) > 0) max = num3;
+        return max;
     }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> testConditions() {
-        return Arrays.asList(new Object[][] {
-
-                // Given Max Number at 1st Position return the Same Number
-                {9, 7, 3, 9},
-                {99, 77, 33, 99},
-                {999, 777, 333, 999},
-                {9999, 7777, 3333, 9999},
-
-                // Given Max Number at 2nd Position return the Same Number
-                {7, 9, 3, 9},
-                {77, 99, 33, 99},
-                {777, 999, 333, 999},
-                {7777, 9999, 3333, 9999},
-
-                // Given Max Number at 3rd Position return the Same Number
-                {7, 3, 9, 9},
-                {77, 33, 99, 99},
-                {777, 333, 999, 999},
-                {7777, 3333, 9999, 9999},
-        });
-    }
-
-    @Test
-    public void testMaxInteger() {
-        assertEquals(output, testMax.maxInteger(num1, num2, num3));
-    }
-
 }
